184ba907a73d7e160dac8da61555e12aa3a4dd71
chetan6780 <crp20802@gmail.com>
2023-05-22 09:38:15 +0530

    update url

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-// const sockets = io('https://bezel-rtc.vercel.app/', { autoConnect: true, forceNew: true });
+// const sockets = io('https://bezel-ui.netlify.app/', { autoConnect: true, forceNew: true });
 // connect to the server
 const sockets = io('/');
 export default sockets;
